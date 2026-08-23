e342a6d315c9683c60d8eaab2da5c4810a4a504f
toppoun <217878558+toppoun@users.noreply.github.com>
2026-08-23 23:33:51 +0900

    default-cpp テンプレートをシンプルに変更

--- assets/templates/default.cpp
+++ assets/templates/default.cpp
@@ -9,16 +9,6 @@ using namespace std;
 
 using ll = long long;
 
-// 1. 見方・状態:
-// 
-// 2. 答えに必要な情報:
-// 
-// 3. 捨てる情報と根拠:
-// 
-// 4. 初期化・更新・判定・計算量:
-// 
-
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
